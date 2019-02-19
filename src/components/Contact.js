a5a11111481502d8cf68d4e76851a688c4d0ee67
Subash Adhikari <subash.ad00@gmail.com>
2019-02-19 11:40:01 +0545

    Contact Component Done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,11 @@
 import React,{Component} from 'react';
-import { Paper, TextField, Typography,MuiThemeProvider,createMuiTheme,FormControl } from '@material-ui/core';
+import { Paper, TextField, Typography,MuiThemeProvider,createMuiTheme,FormControl, Button,Icon } from '@material-ui/core';
 import '../css/contact.css';
 import Hamburger from './Hamburger';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import Snackbar from '@material-ui/core/Snackbar';
+import CustomSnackbarContentWrapper from './CustomSnackbarContentWrapper';
 
 const localTheme = createMuiTheme({
     palette: {
@@ -12,6 +16,21 @@ const localTheme = createMuiTheme({
     typography: { useNextVariants: true },
   });
 export default class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            successSnackbarOpen:false
+        };
+    }
+    submit=(ev)=>{
+        this.setState({ successSnackbarOpen: true });
+    }
+    handleSnackBarClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        this.setState({ open: false });
+      };
     render(){
         return(
             <div className="bgBlue">
@@ -30,10 +49,30 @@ export default class Contact extends Component {
                                     <Typography component="div">
                                         <TextField label="Message" placeholder="Message" fullWidth margin="normal" variant="outlined" multiline rows="4" rowsMax="8" />        
                                     </Typography>
+                                    <Button variant="contained" color="primary" className="button" onClick={this.submit}>
+                                        <FontAwesomeIcon className="rightArrow" icon={faPaperPlane} />
+                                       <span className="sendTxt"> Send   </span>
+                                    </Button>
                                 </FormControl>
+                                
                             </Paper>
                         </MuiThemeProvider>
                 </div>
+                <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                open={this.state.successSnackbarOpen}
+                autoHideDuration={6000}
+                onClose={this.handleSnackBarClose}
+                >
+                    <CustomSnackbarContentWrapper
+                        onClose={this.handleSnackBarClose}
+                        variant="success"
+                        message="Thank you for the email"
+                    />
+                </Snackbar>
             </div>
         )
     }
